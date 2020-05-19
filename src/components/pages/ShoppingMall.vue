bf7e5917ddbb2e78d9a513b02a77add0d298a882
hukai <huzhengen@163.com>
2020-05-19 14:59:45 +0800

    swipe

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="search-bar">
-    <van-row gutter="5">
-      <van-col span="2">
-        <img :src="locationIcon" class="location-icon" />
-      </van-col>
-      <van-col span="18">
-        <!-- <input type="text" class="search-input" /> -->
-        <van-search placeholder="请输入搜索关键词" class="search-input" />
-      </van-col>
-      <van-col span="4">
-        <van-button type="default" size="small">查找</van-button>
-      </van-col>
-    </van-row>
+  <div>
+    <div class="search-bar">
+      <van-row gutter="5">
+        <van-col span="2">
+          <img :src="locationIcon" class="location-icon" />
+        </van-col>
+        <van-col span="18">
+          <!-- <input type="text" class="search-input" /> -->
+          <van-search placeholder="请输入搜索关键词" class="search-input" />
+        </van-col>
+        <van-col span="4">
+          <van-button type="default" size="small">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <div class="swiper-area">
+      <van-swipe :autoplay="5000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+          <img v-lazy="banner.imageUrl" width="100%" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
   </div>
 </template>
 
@@ -19,7 +28,21 @@
 export default {
   data() {
     return {
-      locationIcon: require("../../assets/images/location.png")
+      locationIcon: require("../../assets/images/location.png"),
+      bannerPicArray: [
+        {
+          imageUrl:
+            "https://gw.alicdn.com/imgextra/i2/134/O1CN01QaamV51CrPfrtnutB_!!134-0-lubanu.jpg"
+        },
+        {
+          imageUrl:
+            "https://aecpm.alicdn.com/simba/img/TB1CWf9KpXXXXbuXpXXSutbFXXX.jpg_q50.jpg"
+        },
+        {
+          imageUrl:
+            "https://img.alicdn.com/imgextra/i3/190/O1CN01DnjRIs1DH3qvnfHGg_!!190-0-luban.jpg"
+        }
+      ]
     };
   }
 };
@@ -42,5 +65,9 @@ export default {
 }
 .location-icon {
   max-height: 1.3rem;
+}
+.swiper-area {
+  width: 20rem;
+  clear: both;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Swipe, SwipeItem, Search, Button, Row, Col } from 'vant'
-Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Search)
+import { Lazyload, Swipe, SwipeItem, Search, Button, Row, Col } from 'vant'
+Vue.use(Button).use(Row).use(Col).use(Swipe).use(SwipeItem).use(Search).use(Lazyload)
 
 Vue.config.productionTip = false
 
